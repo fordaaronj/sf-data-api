a582a3dd79554b589e2131fab043722aca198b60
Aaron Ford <fordaaronj@gmail.com>
2020-11-10 20:49:59 -0800

    Update housing query

--- routes/affordability.js
+++ routes/affordability.js
@@ -4,9 +4,13 @@ const router = require('express').Router();
 
 router.get('/housing', async (req, res) => {
     const { rows } = await query(`
-        SELECT extract(year from date) "year", round(avg(percent_households_afford_median_home)) "value"
+        SELECT 
+            extract(year from date) "year", 
+            round(avg(percent_households_afford_median_home))::integer "value"
         FROM housing_affordability
-        WHERE date >= '2010-01-01'
+        WHERE 
+            date >= '2010-01-01'
+            AND region = 'San Francisco'
         GROUP BY 1
         ORDER BY 1
     `, undefined, `housing-affordability`);
